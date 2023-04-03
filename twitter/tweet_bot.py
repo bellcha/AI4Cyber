58c3532d83f67a21ddf3ce59fa028120a8dd0bce
bellcha <bellcha@gmail.com>
2023-04-02 20:09:06 -0500

    more datas

--- twitter/tweet_bot.py
+++ twitter/tweet_bot.py
@@ -11,11 +11,11 @@ bearer_token = config["TWITTER"]["BEARER_TOKEN"]
 
 client = tweepy.Client(bearer_token)
 
-response = client.search_recent_tweets("BlackCat Ransomware", max_results = 100)
+response = client.search_recent_tweets("alphv", max_results = 100)
 
 tweets = response.data
 
-with open('blackcat_tweets.csv', 'w', encoding='utf-8') as w:
+with open('alphv_tweets.csv', 'w', encoding='utf-8') as w:
     w.write('tweet_id,tweet\n')
     for t in tweets:
         t_text = str(t.text).replace('\n',' ')
