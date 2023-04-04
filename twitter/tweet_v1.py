e22639ab03dd3e87e928bb72aaba6b77f14c5592
bellcha <bellcha@gmail.com>
2023-04-04 17:20:18 -0500

    using v1 api

---
+++ twitter/tweet_v1.py
@@ -0,0 +1,41 @@
+import tweepy
+import configparser
+from pathlib import Path
+
+config = configparser.ConfigParser()
+config.read(Path(Path(__file__).parent).joinpath("twitter.ini"))
+
+# Your app's API/consumer key and secret can be found under the Consumer Keys
+# section of the Keys and Tokens tab of your app, under the
+# Twitter Developer Portal Projects & Apps page at
+# https://developer.twitter.com/en/portal/projects-and-apps
+consumer_key = config["TWITTER"]["CONSUMER_KEY"]
+consumer_secret = config["TWITTER"]["CONSUMER_KEY_SECRET"]
+
+# Your account's (the app owner's account's) access token and secret for your
+# app can be found under the Authentication Tokens section of the
+# Keys and Tokens tab of your app, under the
+# Twitter Developer Portal Projects & Apps page at
+# https://developer.twitter.com/en/portal/projects-and-apps
+access_token = config["TWITTER"]["ACCESS_TOKEN"]
+access_token_secret = config["TWITTER"]["ACCESS_TOKEN_SECRET"]
+
+auth = tweepy.OAuth1UserHandler(
+    consumer_key, consumer_secret, access_token, access_token_secret
+)
+
+# If the authentication was successful, this should print the
+# screen name / username of the account
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+# This will search for Tweets with the query "Twitter", returning up to the
+# maximum of 100 Tweets per request to the Twitter API
+
+# Once the rate limit is reached, it will automatically wait / sleep before
+# continuing
+with open('ai4cyber_anonymous_tweets.csv', 'w', encoding='utf-8') as w:
+    w.write('tweet_id,tweet\n')
+    for tweet in tweepy.Cursor(api.search_tweets, "#Anonymous", count=100).items():
+        t_text = str(tweet.text).replace('\n',' ')
+        w.write(f'{tweet.id},{t_text}\n')
